a4e9044e9161b6ef0f3995363611392f96fb3b70
luke <oddthread@gmail.com>
2018-07-03 13:11:03 -0500

    sync

--- www/tapjoyba.js
+++ www/tapjoyba.js
@@ -8,5 +8,5 @@ TapjoyBA.prototype.open = function(appId, callbackContext) {
     exec(callbackContext.success || null, callbackContext.error || null, 'TapjoyBA', 'open', [appId]);
 };
 
-var tapjoyba = new TapjoyBA();
-module.exports = tapjoyba;
+//var tapjoyba = new TapjoyBA();
+//module.exports = tapjoyba;
