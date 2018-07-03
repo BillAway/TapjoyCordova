bc139ae0a192cfb8afbfc21178ae4c5268cd892d
luke <oddthread@gmail.com>
2018-07-03 14:17:26 -0500

    sync

--- www/tapjoyba.js
+++ www/tapjoyba.js
@@ -1,11 +1,10 @@
--var exec = require('cordova/exec');
- -
- -function TapjoyBA() { }
- -
- -TapjoyBA.prototype.open = function(appId, callbackContext) {
- -    callbackContext = callbackContext || {};
- -    exec(callbackContext.success || null, callbackContext.error || null, 'TapjoyBA', 'open', [appId]);
- -};
- -
- -//var tapjoyba = new TapjoyBA();
- -//module.exports = tapjoyba;
+var exec = require('cordova/exec');
+function TapjoyBA() { }
+
+ TapjoyBA.prototype.open = function(appId, callbackContext) {
+     callbackContext = callbackContext || {};
+     exec(callbackContext.success || null, callbackContext.error || null, 'TapjoyBA', 'open', [appId]);
+ };
+ 
+ //var tapjoyba = new TapjoyBA();
+ //module.exports = tapjoyba;
